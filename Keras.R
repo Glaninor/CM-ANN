dcff3bc7c5ca04eeb51b943852099f4054c464c9
Morpheus <51204692+Glaninor@users.noreply.github.com>
2019-08-08 18:07:16 +0800

    Update Keras.R

--- Keras.R
+++ Keras.R
@@ -4,25 +4,41 @@ library(keras)
 library(tensorflow)
 install_tensorflow()
 
+ntrain = sample(nrow(all.judge),floor(0.7*nrow(all.judge)),replace=FALSE)
+train = all.judge[ntrain,]
+test = all.judge[-ntrain,]
+train[which(is.na(train[1,]))]
+train <- train[,-which(names(train)=="mcq240y")]
+train <- train[,-which(names(train)=="mcq240r")]
+train <- train[,-which(names(train)=="mcq240k")]
+train <- train[,-which(names(train)=="mcq240i")]
+train <- train[,-which(names(train)=="mcq240d")]
+train <- train[,-which(names(train)=="mcq230d")]
+test <- test[,-which(names(test)=="mcq240y")]
+test <- test[,-which(names(test)=="mcq240r")]
+test <- test[,-which(names(test)=="mcq240k")]
+test <- test[,-which(names(test)=="mcq240i")]
+test <- test[,-which(names(test)=="mcq240d")]
+test <- test[,-which(names(test)=="mcq230d")]
 
-keras.train <- as.matrix(train[,3:20])
+keras.train <- as.matrix(train[,3:1278])
 keras.train.labels <- as.matrix(train[,2])
-keras.test <- as.matrix(test[,3:20])
+keras.test <- as.matrix(test[,3:1278])
 keras.test.labels <- as.matrix(test[,2])
 
 model <- keras_model_sequential() 
 
 model %>% 
-  layer_dense(units = 32, activation = 'relu', input_shape = c(18)) %>% 
+  layer_dense(units = 64, activation = 'relu', input_shape = c(1276)) %>%
+  layer_dense(units = 32, activation = 'relu') %>%
   layer_dense(units = 1, activation = 'sigmoid') %>% 
   compile( 
     optimizer = optimizer_rmsprop(),
     loss = loss_binary_crossentropy,
-    metrics = metric_binary_accuracy
+    metrics = c('accuracy')
   )
 
-model %>% fit(keras.train,keras.train.labels, epochs=20, batch_size=32)
-score <- model %>% evaluate(keras.test,keras.test.labels, batch_size =32)
+history <- model %>% fit(keras.train,keras.train.labels, epochs=30, batch_size=64)
+model %>% evaluate(keras.test,keras.test.labels)
+plot(history)
 
-score <- model %>% evaluate(keras.test,keras.test.labels, batch_size =32)
-1074/1074 [==============================] - 0s 90us/sample - loss: 5032.7191 - binary_accuracy: 0.6006
